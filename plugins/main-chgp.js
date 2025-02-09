b4150243ac0b7cd0fbb32ff849b8d5d4b2ad222b
Ahmedk1107865 <updatep696@gmail.com>
2025-02-09 15:56:55 +0500

    Update main-chgp.js

--- plugins/main-chgp.js
+++ plugins/main-chgp.js
@@ -1,6 +1,6 @@
 let handler = async m => m.reply(`FOLLOW THIS CHANNEL FOR UPDATES CHAND TRICKER 
 
-*${mssg.link}*: 
+*${mssg.link}*: https://whatsapp.com/channel/0029VaZrEGYIN9ih4PxcFQ33
 
 *𝙊𝙒𝙉𝙀𝙍*
 ${developer}
