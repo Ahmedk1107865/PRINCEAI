c23d3aaf111c1570211d78ef6ded005137298558
Ahmedk1107865 <updatep696@gmail.com>
2025-02-09 15:35:18 +0500

    Update main-ggp.js

--- plugins/main-ggp.js
+++ plugins/main-ggp.js
@@ -1,9 +1,9 @@
 let handler = async m => m.reply(`𝚂𝚄𝙿𝙿𝙾®𝚃 𝙶𝚁𝙾𝚄𝙿 𝙻𝙸𝙽𝙺
  ★☆⚡𝑷-𝑴𝑫⚡☆★
 
-*${mssg.link}*: https://chat.whatsapp.com/I8cEwyPyiwB961alRjB1Bb
+*${mssg.link}*: https://chat.whatsapp.com/HBBWmgTmpNl7OvG2MCpQtR
 
-*${mssg.link}*: https://whatsapp.com/channel
+*${mssg.link}*: https://whatsapp.com/channel/0029VaZrEGYIN9ih4PxcFQ33
 
 *OWNER*
 ${developer}
